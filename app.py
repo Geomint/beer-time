d12f9bda71a0c4c8921fdd0378f13ef5701d4789
George Pyott <44371887+Geomint@users.noreply.github.com>
2019-12-11 20:08:22 +0000

    Added route for contact-us page in app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+##Routes for beer time
 @app.route('/')
 def index():
     return render_template(index.html)
+
+@app.route('/contact')
+def contact():
+    return render_template("contact-us.html")
