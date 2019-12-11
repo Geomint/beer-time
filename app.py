7f40bfadb9592eddeacbef51225036090e1f0249
George Pyott <44371887+Geomint@users.noreply.github.com>
2019-12-11 20:19:45 +0000

    Fixed issue in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 ##Routes for beer time
 @app.route('/')
 def index():
-    return render_template(index.html)
+    return render_template("index.html")
 
 @app.route('/contact')
 def contact():
