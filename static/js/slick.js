be002f636e752c42d57c20ede375f4a242e73dcb
George <george.pyott@googlemail.com>
2019-12-13 15:13:11 +0000

    Added slick files + settings for main banner

---
+++ static/js/slick.js
@@ -0,0 +1,10 @@
+$('.home-banner__slick').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    arrows: false,
+    dots: false,
+    rows: 0
+  });
