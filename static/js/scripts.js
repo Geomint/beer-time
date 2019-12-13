0fc7d0a9e4e4619536307ca8f705b8c98e49ddb8
George <george.pyott@googlemail.com>
2019-12-13 15:59:12 +0000

    Added scroll function to change background colour of nav bar on scroll

---
+++ static/js/scripts.js
@@ -0,0 +1,9 @@
+const header = $('.main-nav');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 0) {
+        header.addClass('main-nav__scrolling');
+    } else {
+        header.removeClass('main-nav__scrolling');
+    }
+  });
