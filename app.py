206299e7868a60e07155bac9f9b8aabc1d3f1687
George Pyott <44371887+Geomint@users.noreply.github.com>
2020-01-05 13:12:50 +0000

    Added body_id to all routes in app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 # Routes for beer time
 @app.route('/')
 def index():
-    return render_template("index.html", page_title="Home")
+    return render_template("index.html", body_id="our-favourites", page_title="Home")
 
 
 @app.route('/whats-hot')
 def hot():
-    return render_template("whats-hot.html", page_title="What's Hot")
+    return render_template("whats-hot.html", body_id="our-favourites", page_title="What's Hot")
 
 
 @app.route('/our-favourites')
@@ -19,7 +19,7 @@ def favourites():
     data = []
     with open("static/data/beers.json", "r") as response:
         data = json.load(response)
-    return render_template("our-favourites.html", page_title="Our Favourites", beers_list=data)
+    return render_template("our-favourites.html", body_id="our-favourites", page_title="Our Favourites", beers_list=data)
 
 
 @app.route('/our-favourites/<beer_name>')
@@ -37,9 +37,9 @@ def about_beer(beer_name):
 
 @app.route('/sign-in')
 def signIn():
-    return render_template("sign-in.html", page_title="Sign In")
+    return render_template("sign-in.html", body_id="our-favourites", page_title="Sign In")
 
 
 @app.route('/contact')
 def contact():
-    return render_template("contact-us.html", page_title="Contact Us")
+    return render_template("contact-us.html", body_id="our-favourites", page_title="Contact Us")
