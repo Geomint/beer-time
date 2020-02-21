a64df9c3fce336bf8afe57c2337061eceaf78043
George Pyott <44371887+Geomint@users.noreply.github.com>
2020-02-21 16:00:11 +0000

    Added jquery to sumbit add-to-fav form when checkbox is ticked

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,13 @@
 const header = $('.main-nav');
 
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(window).scrollTop() > 0) {
         header.addClass('main-nav__scrolling');
     } else {
         header.removeClass('main-nav__scrolling');
     }
-  });
+});
+
+$('.favourite').click(function () {
+    $('.add-to-favourites').submit();
+}) 
