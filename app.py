70e4c2f8515f95fb54f5dd1a60e30c22d84ae1ca
George <george.pyott@googlemail.com>
2020-02-28 11:59:53 +0000

    Removed unused routes + functions from app.py

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ mongo = PyMongo(app)
 
 # Routes for beer time
 
+"""
+This app route is for the index page, this accesses the beers collection in order to output a list of beers 
+onto the slider toward the bottom of the page. 
+"""
+@app.route('/')
+def index():
+    return render_template("index.html", body_id="home-page", page_title="Home")
+
 
 @app.route('/beer/<beer_id>')
 def beer_page(beer_id):
@@ -101,56 +109,6 @@ def insert_beer():
     beers = mongo.db.beers
     beers.insert_one(request.form.to_dict())
     return redirect(url_for('all_beers'))
-
-
-"""
-Route for the home page
-"""
-@app.route('/')
-def index():
-    data = []
-    with open("static/data/beers.json", "r") as response:
-        data = json.load(response)
-    return render_template("index.html", body_id="home-page", page_title="Home", beers_list=data)
-
-
-"""
-Route for the 'whats-hot' page
-"""
-@app.route('/whats-hot')
-def hot():
-    with open("static/data/popular-beers.json", "r") as response:
-        data = json.load(response)
-    return render_template("whats-hot.html", body_id="whats-hot", page_title="What's Hot", beers_list=data)
-
-
-"""
-Route for the 'our-favourites' page
-"""
-@app.route('/our-favourites')
-def favourites():
-    data = []
-    with open("static/data/beers.json", "r") as response:
-        data = json.load(response)
-    return render_template("our-favourites.html", body_id="our-favourites", page_title="Our Favourites", beers_list=data)
-
-
-"""
-Route for the our favourites, individual beer page, the url takes the beer_name and appends it on the url after /our-favourites/
-opening the beers.json file to read and storing that data in 'response' to check that the url is equal to beer_name. If true
-return the beer.html page where the user will see the selected beer.
-"""
-@app.route('/our-favourites/<beer_name>')
-def about_beer(beer_name):
-    beer = {}
-
-    with open("static/data/beers.json", "r") as response:
-        data = json.load(response)
-        for obj in data:
-            if obj["url"] == beer_name:
-                beer = obj
-
-    return render_template("beers/beer.html", beer=beer)
 
 
 @app.route('/my-list', methods=["GET", "POST"])
