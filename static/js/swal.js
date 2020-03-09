cb76f5629e57d48a3c6fa21b020b2b48fd7d886d
George Pyott <44371887+Geomint@users.noreply.github.com>
2020-03-09 20:07:00 +0000

    Created swal.js triggers for edit and delete functions on beers

---
+++ static/js/swal.js
@@ -0,0 +1,42 @@
+const editButton = $('.ud-button-edit');
+const deleteButton = $('.ud-button-delete');
+
+editButton.click(function (e) {
+    e.preventDefault();
+    swal({
+        title: "Are you sure?",
+        text: "You really want to edit this thing?",
+        icon: "warning",
+        buttons: true,
+    })
+        .then((willEdit) => {
+            if (willEdit) {
+                swal("Taking you to the edit page", {
+                    icon: "success",
+                });
+                location.href = this.href
+            } else {
+                swal("You've chosen to leave it as it is!");
+            }
+        });
+})
+
+deleteButton.click(function (e) {
+    e.preventDefault();
+    swal({
+        title: "Are you sure?",
+        text: "Are you super sure you want to delete this?",
+        icon: "warning",
+        buttons: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                swal("Deleted from the database", {
+                    icon: "success",
+                });
+                location.href = this.href
+            } else {
+                swal("You've let this one live!");
+            }
+        });
+})
