8686c9fd889758bb37a59f6a4843cbc5d0fa893b
George Pyott <44371887+Geomint@users.noreply.github.com>
2020-03-11 16:01:37 +0000

    Added try excepts on routes, included password validation on sign in

--- app.py
+++ app.py
@@ -21,7 +21,12 @@ onto the slider toward the bottom of the page.
 '''
 @app.route('/')
 def index():
-    return render_template("index.html", body_id="home-page", page_title="Home")
+    try:
+        current_user = session['username']
+        users = mongo.db.users
+        return render_template("pages/index.html", body_id="home-page", page_title="Home", current_user=users.find_one({'name': session['username']}))
+    except:
+        return render_template("pages/index.html", body_id="home-page", page_title="Home")
 
 
 '''
@@ -47,13 +52,13 @@ def all_beers():
                 current_beer_id = current_beer['_id']
                 favourite_beers_id.append(current_beer_id)
 
-        return render_template("beers/all-beers.html", favourite_beers_id=favourite_beers_id, beers=mongo.db.beers.find(), body_id="all-beers", current_user=users.find_one({'name': session['username']}))
+        return render_template("pages/beers/all-beers.html", favourite_beers_id=favourite_beers_id, beers=mongo.db.beers.find(), body_id="all-beers", current_user=users.find_one({'name': session['username']}))
     except:
-        return redirect(url_for('createAccount'))
+        return redirect(url_for('create_account'))
 
 
 @app.route('/my-list', methods=["GET", "POST"])
-def myList():
+def my_list():
     current_user = session['username']
     users = mongo.db.users
     current_user_obj = users.find_one({'name': session['username']})
@@ -71,7 +76,7 @@ def myList():
         current_beer = mongo.db.beers.find_one({'_id': fav})
         favourite_beers.append(current_beer)
 
-    return render_template("my-list.html", body_id="my-list", page_title="My List", favourite_beers_id=favourite_beers_id, favourite_beers=favourite_beers, current_user=users.find_one({'name': session['username']}))
+    return render_template("pages/my-list.html", body_id="my-list", page_title="My List", favourite_beers_id=favourite_beers_id, favourite_beers=favourite_beers, current_user=users.find_one({'name': session['username']}))
 
 
 @app.route('/add-to-fav/<beer_id>', methods=['POST'])
@@ -82,7 +87,7 @@ def addToFavourites(beer_id):
     current_user_favourites = current_user_obj['favourites']
     mongo.db.users.update(
         current_user_obj, {"$push": {"favourites": ObjectId(beer_id)}})
-    return redirect(url_for('myList'))
+    return redirect(url_for('my_list'))
 
 
 @app.route('/remove-from-favourites/<beer_id>', methods=['POST'])
@@ -93,19 +98,21 @@ def remove_from_favourites(beer_id):
     current_user_favourites = current_user_obj['favourites']
     mongo.db.users.update(
         current_user_obj, {"$pull": {"favourites": ObjectId(beer_id)}})
-    return redirect(url_for('myList'))
+    return redirect(url_for('my_list'))
 
 
 @app.route('/beer/<beer_id>')
 def beer_page(beer_id):
+    current_user = session['username']
+    users = mongo.db.users
     the_beer = mongo.db.beers.find_one({"_id": ObjectId(beer_id)})
     you_might_like = mongo.db.beers.find().limit(3)
-    return render_template('beers/beer.html', beer=the_beer, you_might_like=you_might_like, body_id="beer-product")
+    return render_template('pages/beers/beer.html', beer=the_beer, you_might_like=you_might_like, body_id="beer-product", current_user=users.find_one({'name': session['username']}))
 
 
 @app.route('/add-beer')
 def add_beer():
-    return render_template('beers/add-beer.html', body_id="add-beer", types=mongo.db.types.find())
+    return render_template('pages/beers/add-beer.html', body_id="add-beer", types=mongo.db.types.find())
 
 
 @app.route('/insert-beer', methods=['POST'])
@@ -117,12 +124,14 @@ def insert_beer():
 
 @app.route('/edit-beer/<beer_id>')
 def edit_beer(beer_id):
+    current_user = session['username']
+    users = mongo.db.users
     the_beer = mongo.db.beers.find_one({"_id": ObjectId(beer_id)})
     all_types = mongo.db.types.find()
-    return render_template('beers/edit-beer.html', beer=the_beer, types=all_types)
+    return render_template('pages/beers/edit-beer.html', body_id='edit-page', beer=the_beer, types=all_types, current_user=users.find_one({'name': session['username']}))
 
 
-@app.route('/update_beer/<beer_id>', methods=['POST'])
+@app.route('/update-beer/<beer_id>', methods=['POST'])
 def update_beer(beer_id):
     beer = mongo.db.beers
     beer.update({'_id': ObjectId(beer_id)},
@@ -146,26 +155,28 @@ def delete_beer(beer_id):
 
 
 @app.route('/register', methods=["GET", "POST"])
-def createAccount():
+def create_account():
     if request.method == 'POST':
         users = mongo.db.users
         existing_user = users.find_one({'name': request.form['username']})
         favourites_array = []
+        password = request.form['password']
+        repeat_password = request.form['repeat_password']
 
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(
-                request.form['password'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({
-                'name': request.form['username'],
-                'password': hashpass,
-                'favourites': favourites_array
-            })
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-
-        return 'That Username Already Exists!'
-
-    return render_template("create-account.html", body_id="register-page", page_title="Create an Account")
+        if password == repeat_password:
+            if existing_user is None:
+                hashpass = bcrypt.hashpw(
+                    request.form['password'].encode('utf-8'), bcrypt.gensalt())
+                users.insert({
+                    'name': request.form['username'].lower(),
+                    'password': hashpass,
+                    'favourites': favourites_array
+                })
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+            flash('That username already exists, try something else.')
+        flash('The passwords dont match.')
+    return render_template("pages/account-nav.html", body_id="register-page", page_title="Create an Account")
 
 
 """
@@ -175,19 +186,20 @@ Route for the sign-in page
 
 @app.route('/sign-in', methods=["POST", "GET"])
 def sign_in():
-    return render_template("sign-in.html", body_id="sign-in", page_title="Sign In")
+    return render_template("pages/account-nav.html", body_id="sign-in", page_title="Sign In")
 
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
     users = mongo.db.users
-    login_user = users.find_one({'name': request.form['username']})
+    login_user = users.find_one({'name': request.form['username'].lower()})
 
     if login_user:
         if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
             return redirect(url_for('index'))
-    return 'invalid user/pass'
+    flash('That username/password combination was incorrect')
+    return redirect(url_for('sign_in'))
 
 
 """
@@ -204,10 +216,18 @@ Route for the contact page
 """
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
-    if request.method == "POST":
-        flash("Thanks {} we have recieved your message!".format(
-            request.form["name"]))
-    return render_template("contact-us.html", body_id="contact-page", page_title="Contact Us")
+    try:
+        current_user = session['username']
+        users = mongo.db.users
+        if request.method == "POST":
+            flash("Thanks {} we have recieved your message!".format(
+                request.form["name"]))
+        return render_template("pages/contact-us.html", body_id="contact-page", page_title="Contact Us", current_user=users.find_one({'name': session['username']}))
+    except:
+        if request.method == "POST":
+            flash("Thanks {} we have recieved your message!".format(
+                request.form["name"]))
+        return render_template("pages/contact-us.html", body_id="contact-page", page_title="Contact Us")
 
 
 if __name__ == '__main__':
