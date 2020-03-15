282a2eaf790cd1e4596acac2aef750ac8e179013
George Pyott <44371887+Geomint@users.noreply.github.com>
2020-03-15 20:31:46 +0000

    Added JS to handle open+close of hidden nav

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -20,3 +20,13 @@ $('.checkedBox').attr('checked', true);
 
 
 $('[data-toggle="tooltip"]').tooltip();
+
+//nav open button
+$('.nav-button').on('click', function() {
+    $('html').addClass('open-nav show-overlay');
+});
+
+//click to close
+$('.overlay, .mobile-nav').on('click', function(){
+    $('html').removeClass('open-nav show-overlay')
+});
