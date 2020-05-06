969bbb1b5073909af959468a80b77fcee9959c1a
George <george.pyott@googlemail.com>
2020-05-06 14:03:34 +0100

    Beautify scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,13 +1,13 @@
 const header = $('.main-nav');
 const reviewSlideSelector = $('#review-selector')
 
-reviewSlideSelector.click(function(){
+reviewSlideSelector.click(function () {
     $(".beer-page__review__form").show();
     reviewSlideSelector.hide();
     return false;
 })
 
-$('#edit-review').click(function(){
+$('#edit-review').click(function () {
     $(".beer-page__review__form__edit").show();
     reviewSlideSelector.hide();
     return false;
@@ -21,11 +21,11 @@ $(window).scroll(function () {
     }
 });
 
-$('.favourite').click(function() {
+$('.favourite').click(function () {
     $('.add-to-favourites').submit();
 })
 
-$('.checkedBox').click(function() {
+$('.checkedBox').click(function () {
     $('.remove-from-favourites').submit();
 })
 
@@ -35,11 +35,11 @@ $('.checkedBox').attr('checked', true);
 $('[data-toggle="tooltip"]').tooltip();
 
 //nav open button
-$('.nav-button').on('click', function() {
+$('.nav-button').on('click', function () {
     $('html').addClass('open-nav show-overlay');
 });
 
 //click to close
-$('.overlay, .mobile-nav').on('click', function(){
+$('.overlay, .mobile-nav').on('click', function () {
     $('html').removeClass('open-nav show-overlay')
 });
